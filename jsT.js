42eb14a4ccf26d55c9723916378341c54e56cb1b
Almaz <akhadiev@phoenixit.ru>
2023-07-20 13:59:47 +0300

    insert question

--- jsT.js
+++ jsT.js
@@ -6,6 +6,13 @@ const personalMovieDB= {
     genres: [],
     privat: false
 };
+if (numberOfFilms===1){
+    console.log('мало');
+}
+else{
+    console.log('норм так');
+}
 const first=prompt("Один из последних фильмоыв?");
 const second=prompt("На сколько оцените его?");
 personalMovieDB.movies[first] = second;
+console.log(personalMovieDB);
