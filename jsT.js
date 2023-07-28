c112706bb1a962249c38966bd4339619074d7825
Almaz <akhadiev@phoenixit.ru>
2023-07-28 14:16:44 +0300

    insert function

--- jsT.js
+++ jsT.js
@@ -1,4 +1,13 @@
-const numberOfFilms=+prompt("Сколько фильмов вы уже посмотрели?");
+let numberOfFilms;
+function start(){
+    numberOfFilms=+prompt("Сколько фильмов вы уже посмотрели?");
+
+    while (numberOfFilms == '' || numberOfFilms == null || isNaN(numberOfFilms)) {
+        numberOfFilms=+prompt("Сколько фильмов вы уже посмотрели?");
+    }
+}
+start ();
+
 const personalMovieDB= {
     count: numberOfFilms,
     movies: {},
@@ -7,28 +16,51 @@ const personalMovieDB= {
     privat: false
 };
 
-
-for(let i=0;i<2;i++){
-    const first=prompt("Один из последних фильмоыв?");
-    const second=prompt("На сколько оцените его?");
-
-    if(first!=null && second!=null && first!=''&&second!=''&&first.length<50){
-        personalMovieDB.movies[first] = second;
-        console.log('success');
-    } else{
-        console.log('error');
-        i--;
+function rememberMyFilms(){
+    for(let i=0;i<2;i++){
+        const first=prompt("Один из последних фильмоыв?");
+        const second=prompt("На сколько оцените его?");
+    
+        if(first!=null && second!=null && first!=''&&second!=''&&first.length<50){
+            personalMovieDB.movies[first] = second;
+            console.log('success');
+        } else{
+            console.log('error');
+            i--;
+        }
+        
+        
     }
-    
-    
 }
-if(personalMovieDB.count <10){
-    console.log('Мало');
-}else if(personalMovieDB.count>10 && personalMovieDB.count<30){
-    console.log('офигеть');
-}else if(personalMovieDB.count>=30){
-    console.log('жесткий');
-}else{
-    console.log('ошибка');
+rememberMyFilms();
+
+
+
+
+function detect(){
+    if(personalMovieDB.count <10){
+        console.log('Мало');
+    }else if(personalMovieDB.count>10 && personalMovieDB.count<30){
+        console.log('офигеть');
+    }else if(personalMovieDB.count>=30){
+        console.log('жесткий');
+    }else{
+        console.log('ошибка');
+    }
 }
+detect();
+function showMyDb(topd){
+    if(!topd){
+        console.log(personalMovieDB);
+    }
+}
+showMyDb(personalMovieDB.privat);
 console.log(personalMovieDB);
+
+function writeYG(){
+    for(let i=1;i<=3;i++){
+        const genre=prompt(`Ваш любимы жанер под номером ${i}`);
+        personalMovieDB.genres[i-1]=genre;
+    }
+}
+writeYG();
